054179c289cba6255b59a5510dfaa4b54d70a35b
KevinOK15 <kevin771515@gmail.com>
2023-09-24 10:28:35 -0500

    Se agrega el Proyecto

---
+++ Yogurt/src/semana05/Yogurt.java
@@ -0,0 +1,166 @@
+package semana05;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+import javax.swing.JTextField;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.UIManager;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
+
+import java.awt.Font;
+
+public class Yogurt extends JFrame implements ActionListener {
+
+	// Declaración de variables
+	private static final long serialVersionUID = 9206324162700448001L;
+	private JPanel contentPane;
+	private JLabel lblMarca;
+	private JLabel lblCantidad;
+	private JComboBox<String> cboMarca;
+	private JTextField txtCantidad;
+	private JButton btnProcesar;
+	private JButton btnBorrar;
+	private JScrollPane scpScroll;
+	private JTextArea txtS;
+
+	// Lanza la aplicación
+	public static void main(String[] args) {
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		}
+		catch (Throwable e) {
+			e.printStackTrace();
+		}
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Yogurt frame = new Yogurt();
+					frame.setVisible(true);
+				}
+				catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	// Crea la GUI
+	public Yogurt() {
+		setTitle("Yogurt");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+
+		lblMarca = new JLabel("Marca");
+		lblMarca.setBounds(10, 11, 87, 14);
+		contentPane.add(lblMarca);
+
+		lblCantidad = new JLabel("Cantidad");
+		lblCantidad.setBounds(10, 36, 80, 14);
+		contentPane.add(lblCantidad);
+
+		cboMarca = new JComboBox<String>();
+		cboMarca.setModel(new DefaultComboBoxModel<String>(
+				new String[] { "Laive", "Gloria", "Pura Vida", "Milkito" }));
+		cboMarca.setBounds(107, 8, 100, 20);
+		contentPane.add(cboMarca);
+
+		txtCantidad = new JTextField();
+		txtCantidad.setBounds(107, 33, 100, 20);
+		contentPane.add(txtCantidad);
+		txtCantidad.setColumns(10);
+
+		btnProcesar = new JButton("Procesar");
+		btnProcesar.addActionListener(this);
+		btnProcesar.setBounds(335, 7, 89, 23);
+		contentPane.add(btnProcesar);
+
+		btnBorrar = new JButton("Borrar");
+		btnBorrar.addActionListener(this);
+		btnBorrar.setBounds(335, 32, 89, 23);
+		contentPane.add(btnBorrar);
+
+		scpScroll = new JScrollPane();
+		scpScroll.setBounds(10, 61, 414, 190);
+		contentPane.add(scpScroll);
+
+		txtS = new JTextArea();
+		txtS.setFont(new Font("Monospaced", Font.PLAIN, 12));
+		scpScroll.setViewportView(txtS);
+	}
+
+	// Direcciona eventos de tipo ActionEvent
+	public void actionPerformed(ActionEvent arg0) {
+		if (arg0.getSource() == btnBorrar) {
+			actionPerformedBtnBorrar(arg0);
+		}
+		if (arg0.getSource() == btnProcesar) {
+			actionPerformedBtnProcesar(arg0);
+		}
+	}
+
+	// Procesa la pulsación del botón Borrar
+	protected void actionPerformedBtnBorrar(ActionEvent arg0) {
+		txtCantidad.setText("");
+		txtS.setText("");
+		cboMarca.setSelectedIndex(0);
+		txtCantidad.requestFocus();
+	}
+
+	// Procesa la pulsación del botón Procesar
+	protected void actionPerformedBtnProcesar(ActionEvent arg0) {
+		int cant, cara;
+		double prec, impC, impD, impP;
+		String marc;
+		cant = Integer.parseInt( txtCantidad.getText());
+		marc = cboMarca.getSelectedItem().toString();
+		
+		if (marc == "Laive") { prec=5.4;}
+		else if (marc == "Gloria") { prec=5.7;}
+		else if (marc == "Pura Vida") { prec=4.8;}
+		else { prec=5.3;}
+		impC = prec*cant;
+		
+		if (cant<3){ impD = 0;}
+		else if (cant>=3 && cant<6){ impD = impC*0.025;}
+		else if (cant>=6 && cant<12){ impD = impC*0.05;}
+		else { impD = impC*0.075;}
+		impP = impC - impD;
+		
+		if (marc=="Laive"){cara=2*cant;}
+		else if (marc=="Gloria"){cara=3*cant;}
+		else if (marc=="Pura Vida"){cara=4*cant;}
+		else {cara=2*cant;}
+		
+		
+		txtS.setText("SISTEMA DE YOGURT \n\n");
+		txtS.append("Importe compra :" + decimalFormat(impC) + "\n");
+		txtS.append("Importe descuento :" + decimalFormat(impD) + "\n");
+		txtS.append("Importe a pagar :" + decimalFormat(impP) + "\n");
+		txtS.append("Caramelos :" + cara + "\n");
+		
+	}
+	String decimalFormat(double p){
+		return String.format("%.2f", p).replace(",", ".");
+}
+	}
+
+
+
+
+
